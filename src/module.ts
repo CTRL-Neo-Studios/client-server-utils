08e5194f7025fa26e0ccc82d52ad7a8545730673
Type-32 <87076491+Type-32@users.noreply.github.com>
2026-02-23 23:35:12 +0800

    feat: Add parsing utils to auto-imports and aliases

--- src/module.ts
+++ src/module.ts
@@ -1,30 +1,39 @@
 import {
-  defineNuxtModule,
-  addPlugin,
-  createResolver,
-  addImportsDir,
-  addServerImportsDir
+	defineNuxtModule,
+	createResolver,
+	addImportsDir,
+	addServerImportsDir
 } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+}
 
 export default defineNuxtModule<ModuleOptions>({
-  meta: {
-    name: '@type32/nuxt-cs-utils',
-    configKey: 'csUtils',
-  },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+	meta: {
+		name: '@type32/nuxt-cs-utils',
+		configKey: 'csUtils',
+	},
+	// Default configuration options of the Nuxt module
+	defaults: {},
+	setup(_options, _nuxt) {
+		const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addImportsDir(resolver.resolve('runtime/shared/utils'))
-    addServerImportsDir(resolver.resolve('runtime/shared/utils'))
+		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+		addImportsDir(resolver.resolve('runtime/shared/utils'))
+		addImportsDir(resolver.resolve('runtime/shared/utils/parsing'))
 
-    _nuxt.options.alias['@type32/nuxt-cs-utils'] = resolver.resolve(
-      './runtime/shared/utils',
-    )
-  },
+		// Nitro's directory scanner is not recursive by default, so each subdirectory
+		// must be registered explicitly for server-side auto-imports to work.
+		addServerImportsDir(resolver.resolve('runtime/shared/utils'))
+		addServerImportsDir(resolver.resolve('runtime/shared/utils/parsing'))
+
+		_nuxt.options.alias['@type32/nuxt-cs-utils'] = resolver.resolve(
+			'./runtime/shared/utils',
+		)
+
+		_nuxt.options.alias['@type32/nuxt-cs-utils'] = resolver.resolve(
+			'./runtime/shared/utils/parsing',
+		)
+	},
 })
